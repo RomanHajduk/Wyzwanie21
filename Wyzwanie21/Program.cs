1b7c5f6f08b525c4a0275ff2132d1fff5fb0c1cd
Roman Hajduk <rhajduk@poczta.fm>
2024-03-03 16:25:43 +0100

    Klasa pracownik i lista pracowników z największym wynikiem

--- Wyzwanie21/Program.cs
+++ Wyzwanie21/Program.cs
@@ -1,37 +1,63 @@
-﻿//deklaracja zmiennych
-var numberToCheck = 343477;
-char[] letters = numberToCheck.ToString().ToArray();
-char[] numberToLetter = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+﻿using Wyzwanie21;
 
-// pierwszy sposób
-Console.WriteLine("Liczba do sprawdzenia: " + numberToCheck);
+var prac1 = new Employee("Mariusz", "Kolonko", 32);
+var prac2 = new Employee("Ryszard", "Ochódzki", 50);
+var prac3 = new Employee("Mandaryna", "Wiśniewska", 40);
 
-foreach (var number in numberToLetter)
+prac1.AddScore(7);
+prac1.AddScore(3);
+prac1.AddScore(4);
+prac1.AddScore(5);
+prac1.AddScore(10);
+
+prac2.AddScore(10);
+prac2.AddScore(7);
+prac2.AddScore(10);
+prac2.AddScore(2);
+prac2.AddScore(8);
+
+prac3.AddScore(10);
+prac3.AddScore(11);
+prac3.AddScore(4);
+prac3.AddScore(5);
+prac3.AddScore(8);
+
+List<Employee> employees = new List<Employee>();
+employees.Add(prac1);
+employees.Add(prac2);
+employees.Add(prac3);
+
+int Count = 0;
+int maxPoint = 0;
+foreach (var employee in employees)
 {
-    var count = 0;
-    foreach (var letter in letters)
+    if (maxPoint < employee.Result)
     {
-        if (number == letter)
-            count++;
+        maxPoint = employee.Result;
+        Count = employees.IndexOf(employee);
     }
-    Console.WriteLine(number + "=> " + count);
 }
+Console.WriteLine("Pracownik " + employees.ElementAt(Count).Name + " " + employees.ElementAt(Count).LastName + " " +
+                  "z wiekiem " + employees.ElementAt(Count).Age + " lat ma największą liczbę punktów czyli: " + employees.ElementAt(Count).Result);
+// drugi sposób - ten lepszy bo uwzględnia wszystkich mających taką samą największą liczbę punktów
+int maxPoint2 = 0;
 
-//drugi sposób
-Console.WriteLine("drugi sposób z listą");
-
-// deklaracja listy
-List<char> numbers = new List<char>();
-for (int i = 0; i < 10; i++)
-    numbers.Add(i.ToString()[0]);
-
-foreach (var number in numbers)
+List<Employee> employeesWithMaxScore = new List<Employee>();
+foreach (var employee in employees)
 {
-    var count = 0;
-    foreach (var letter in letters)
+    if (maxPoint2 <= employee.Result)
     {
-        if (number == letter)
-            count++;
+        maxPoint2 = employee.Result;
     }
-    Console.WriteLine(number + "=> " + count);
+}
+Console.WriteLine();
+Console.WriteLine("Drugi sposób, który wyświetla wszystkich mających największą sumę punktów, gdyby się tak zdarzyło!");
+Console.WriteLine("Najwięcej punktów mają:");
+foreach (var employee in employees)
+{
+    if (maxPoint2 == employee.Result)
+    {
+        Console.WriteLine("Pracownik " + employee.Name + " " + employee.LastName + " lat: " + employee.Age + " ma punktów: " + employee.Result);
+    }
+
 }
