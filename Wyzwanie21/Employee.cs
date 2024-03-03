2ca23022822635844a8058ef64b6be0d3e55b735
Roman Hajduk <rhajduk@poczta.fm>
2024-03-03 16:25:43 +0100

    Klasa pracownik i lista pracowników z największym wynikiem

---
+++ Wyzwanie21/Employee.cs
@@ -0,0 +1,29 @@
+﻿namespace Wyzwanie21
+{
+    internal class Employee
+    {
+        public string Name { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        List<int> Scores = new List<int>();
+
+        public Employee(string Name, string LastName, int Age)
+        {
+            this.Name = Name;
+            this.LastName = LastName;
+            this.Age = Age;
+        }
+
+        public int Result
+        {
+            get
+            {
+                return this.Scores.Sum();
+            }
+        }
+        public void AddScore(int Score)
+        {
+            this.Scores.Add(Score);
+        }
+    }
+}
