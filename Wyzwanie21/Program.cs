cca377bd39f29195274fb2a28c77577a8740fe72
Roman Hajduk <rhajduk@poczta.fm>
2024-03-03 15:45:17 +0100

    Zadanie z warunkami

--- Wyzwanie21/Program.cs
+++ Wyzwanie21/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Witajcie w programistycznej bajce!");
+﻿
+// deklaracja imienia
+var name = "Ewa";
+// zmienna logiczna, prawda jeśli jest kobietą, false jeśli jest mężczyzną 
+var isWoman = false;
+//zmienna przechowująca wiek
+var age = 18;
+
+if (isWoman && (age < 30))
+    Console.WriteLine("Kobieta poniżej 30 lat");
+else if (name == "Ewa" && age == 30)
+    Console.WriteLine("Ewa, lat 30");
+
+else if (!isWoman && age < 18)
+    Console.WriteLine("Niepełnoletni mężczyzna");
+else 
+    Console.WriteLine("nie pasuje do podanych kryteriów");
